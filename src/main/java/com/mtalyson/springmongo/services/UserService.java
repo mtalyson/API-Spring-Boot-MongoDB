87aafa4410bf78bb0c35da69cd0e90a06b998b4d
Talyson Moreira <talyson.penha@gmail.com>
2021-01-14 15:57:32 -0300

    Getting a user by ID

--- src/main/java/com/mtalyson/springmongo/services/UserService.java
+++ src/main/java/com/mtalyson/springmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.mtalyson.springmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mtalyson.springmongo.domain.User;
 import com.mtalyson.springmongo.repository.UserRepository;
+import com.mtalyson.springmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
