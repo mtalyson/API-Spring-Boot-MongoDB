7d72a59ebd5c18a66dc765a41b7b4ef8847415f9
Talyson Moreira <talyson.penha@gmail.com>
2021-01-14 20:15:23 -0300

    Getting post by Id

---
+++ src/main/java/com/mtalyson/springmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.mtalyson.springmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mtalyson.springmongo.domain.Post;
+import com.mtalyson.springmongo.repository.PostRepository;
+import com.mtalyson.springmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
