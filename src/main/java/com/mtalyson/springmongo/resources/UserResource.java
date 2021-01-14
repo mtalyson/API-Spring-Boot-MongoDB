913e4095267909bf53a6255e69bcf1338197a565
Talyson Moreira <talyson.penha@gmail.com>
2021-01-14 15:32:24 -0300

    Using DTO to return users

--- src/main/java/com/mtalyson/springmongo/resources/UserResource.java
+++ src/main/java/com/mtalyson/springmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.mtalyson.springmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mtalyson.springmongo.domain.User;
+import com.mtalyson.springmongo.dto.UserDTO;
 import com.mtalyson.springmongo.services.UserService;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 }
