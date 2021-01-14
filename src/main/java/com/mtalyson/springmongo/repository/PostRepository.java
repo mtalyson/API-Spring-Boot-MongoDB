516f78dece37ee6a3e174b6d45e7083f4ef672be
Talyson Moreira <talyson.penha@gmail.com>
2021-01-14 17:47:03 -0300

    POST entity creation with nested user

--- src/main/java/com/mtalyson/springmongo/repository/PostRepository.java
+++ src/main/java/com/mtalyson/springmongo/repository/PostRepository.java
@@ -3,9 +3,9 @@ package com.mtalyson.springmongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.mtalyson.springmongo.domain.User;
+import com.mtalyson.springmongo.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
-
+public interface PostRepository extends MongoRepository<Post, String> {
+	
 }
